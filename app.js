4dcb19ff6240b675120964f7e5c43dfcdd1388b1
Marlie Martinez <marlieahue@gmail.com>
2024-09-15 14:37:21 -0400

    css

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const playBtn = document.getElementById('playBtn');
 
 
-const wavesurfer = WaveSurfer.create({
+const wavesurfer1 = WaveSurfer.create({
     container: '#waveform',
     waveColor: '#4F4A85',
     progressColor: '#383351',
@@ -11,17 +11,42 @@ const wavesurfer = WaveSurfer.create({
     barRadius: 4,
   })
 
-  wavesurfer.load('./audio/fsdfvs.wav');
+  wavesurfer1.load('./audio/goodbye.mp3');
 
   const togglePlayPause = () => {
-    wavesurfer.playPause();
-    playBtn.src = wavesurfer.isPlaying() 
+    wavesurfer1.playPause();
+    playBtn.src = wavesurfer1.isPlaying() 
       ? './img/pause.png'
       : './img/play-button-icon-png-18919.png';
   };
   
   playBtn.addEventListener('click', togglePlayPause);
   
-  wavesurfer.on('finish', () => {
+  wavesurfer1.on('finish', () => {
     playBtn.src = './img/play-button-icon-png-18919.png';
   });
+  
+//   const wavesurfer2 = WaveSurfer.create({
+//     container: '#waveform2',
+//     waveColor: '#4F4A85',
+//     progressColor: '#383351',
+//     barWidth: 4,
+//     responsive: true,
+//     height: 90,
+//     barRadius: 4,
+//   })
+
+//   wavesurfer2.load('./audio/fsdfvs.wav');
+
+//   const togglePlayPause2 = () => {
+//     wavesurfer2.playPause();
+//     playBtn.src = wavesurfer2.isPlaying() 
+//       ? './img/pause.png'
+//       : './img/play-button-icon-png-18919.png';
+//   };
+
+//   playBtn.addEventListener('click', togglePlayPause2);
+
+//   wavesurfer2.on('finish', () => {
+//     playBtn.src = './img/play-button-icon-png-18919.png';
+//   });
